9cdd26107f26836eb7efc4ce9b2f407cfefca633
Akaikenlol <67327997+Akaikenlol@users.noreply.github.com>
2020-07-05 14:55:50 +0630

    Add files via upload

---
+++ In/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace In
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TextReader tIn = Console.In;
+            TextWriter tOut = Console.Out;
+
+            tOut.WriteLine("Keith");
+            tOut.Write("What is your Name:");
+            string name = tIn.ReadLine();
+
+            tOut.WriteLine("Keith,{4}!",name);
+        }
+    }
+}
